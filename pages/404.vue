e5cce12057bce380dfe59da5af97ccbc113b046b
Nam Truong <namt@sintelix.com>
2021-07-07 19:57:19 +0700

    fix broken 404 display

---
+++ pages/404.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- Your template -->
+  <div>
+      <h1>Page not found. Please go <NuxtLink to="/">Home</NuxtLink></h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    // props: ['error'],
+    // layout: 'error'
+  }
+</script>
