31724a368441734bd63898c9d6d4e62e3f7146b3
Nam Truong <namt@sintelix.com>
2021-07-09 17:24:56 +0700

    fix broken display of page 404 due to wrong path of src-img

--- docs/_nuxt/static/1625823104/Home/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1625823104",layout:"default",error:a,state:{counter:0,person:{name:"John",age:45}},serverRendered:true,routePath:"\u002FHome",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9094016b"}}}}(null));
---
+++ docs/_nuxt/static/1625826025/Home/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1625826025",layout:"default",error:a,state:{counter:0,person:{name:"John",age:45}},serverRendered:true,routePath:"\u002FHome",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0d6c934b"}}}}(null));
