38ac28725e7b84e0113e969b46008aca4041d99f
Nam Truong <nam.truong@macbook2019.pro>
2025-01-15 13:29:32 +1030

    add required files in docs/_nuxt/*

---
+++ docs/_nuxt/d8c3add.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{501:function(n,l,t){"use strict";t.r(l);var o={layout:"blog"},e=t(71),component=Object(e.a)(o,(function(){return(0,this._self._c)("div",[this._v("\n    Hello Nam!\n")])}),[],!1,null,null,null);l.default=component.exports}}]);
